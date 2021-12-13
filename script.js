74c8496c34f13d7681ac5f936bf24b38a9c73ff7
Luiz Bonini <lc.bonini13@gmail.com>
2021-12-13 10:56:04 -0300

    Correção de Erros

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-const balls = document.querySelector('.balls')
-const quant = document.querySelectorAll('.slides image')
+let balls = document.querySelector('.balls')
+let quant = document.querySelectorAll('.slides .image')
 let atual = 0
 let imagem = document.getElementById('atual')
 let next = document.getElementById('next')
 let voltar = document.getElementById('voltar')
+let rolar = true
 
+/* ======= Adicionar bolas com a quantidade de imagens ===== */
 for (let i=0; i < quant.length; i++) {
     let div = document.createElement('div')
     div.id = i
@@ -19,15 +21,18 @@ for (let i = 0; i < pos.length; i++) {
     pos[i].addEventListener('click', () => {
         atual = pos[i].id
         slide()
+        rolar = false
     })
 }
 
 voltar.addEventListener('click', () => {
     atual--
+    rolar = false
     slide()
 })
 next.addEventListener('click', () => {
     atual++
+    rolar = false
     slide()
 })
 
@@ -37,9 +42,16 @@ function slide() {
     } else if(atual < 0) {
         atual = quant.length - 1
     }
-    document.querySelector('.img_atual').classList.remove('.img_atual')
+    document.querySelector('.img_atual').classList.remove('img_atual')
     imagem.style.marginLeft = -500*atual+'px'
     document.getElementById(atual).classList.add('img_atual')
 }
 
-slide(0)
+setInterval (()=>{
+    if(rolar){
+        atual++
+        slide()
+    } else {
+        rolar = true
+    }
+},4000)
